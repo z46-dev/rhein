5003efc644682a083ea9378a6cef79f574e87bd1
z46 <irish5165@aol.com>
2022-10-13 10:10:12 -0400

    More comments and bug fixes and stuff

--- compiler.js
+++ compiler.js
@@ -24,10 +24,10 @@ const types = {
 const returnType = "any";
 
 let operators = {
-    "+": (a, b) => [a, b].join(" + "),
-    "-": (a, b) => [a, b].join(" - "),
-    "*": (a, b) => [a, b].join(" * "),
-    "/": (a, b) => [a, b].join(" / ")
+    "+": (a, b) => `${a} + ${b}`, //[a, b].join(" + "),
+    "-": (a, b) => `${a} - ${b}`, //[a, b].join(" - "),
+    "*": (a, b) => `${a} * ${b}`, //[a, b].join(" * "),
+    "/": (a, b) => `${a} / ${b}` //[a, b].join(" / ")
 };
 
 class Variable {
@@ -40,12 +40,22 @@ class Variable {
 
 // Get stuff that's in the string until the search
 function getUntilNext(code, search) {
+    // Turn the code to a string
     code = code.join(" ");
+
+    // Find the code
     let index = code.indexOf(search);
+
+    // If we cannot find our query...
     if (index === -1) {
+        // Then return a -1 to signify as such
         return -1;
     }
+
+    // Get the code that's in the query
     let result = code.slice(0, index);
+
+    // Return the new code, and the code we took out
     return [code.replace(result + search, "").split(" "), result];
 }
 
@@ -109,7 +119,7 @@ function getValue(value, variables, typeName) {
 
             // Apply value
             realValue = operators[value[i]](realValue, value[i + 1]);
-            
+
             // Skip over the value we just read.
             i++;
         } else {
@@ -120,28 +130,52 @@ function getValue(value, variables, typeName) {
     return realValue;
 }
 
+// The main compiling function, can be called in itself to compile labels
 function compile(code, variables = {}, indents = 0, inLabel = false, isTypeStrict = true) {
+    // Trim the code up
     code = code.split("\n").join(" ").trim();
+
+    // The typeStrict check
     if (code.startsWith("@typeStrict=")) {
+        // Get itttt bestie
         let stopIndex = code.indexOf(";");
+
+        // If it's invalid as a configuration statement, invade Antarctica
         if (stopIndex === -1) {
             throw new Error("Invalid typeStrict statement!");
         }
+
+        // Get the raw value
         isTypeStrict = code.slice(12, stopIndex);
+
+        // Convert it to a boolean
         if (isTypeStrict == "false" || isTypeStrict === "no") {
             isTypeStrict = false;
         } else if (isTypeStrict == "true" || isTypeStrict === "yes") {
             isTypeStrict = true;
         }
+
+        // Replace this sequence in the code
         code = code.slice(stopIndex + 1);
     }
+
+    // Trim up and split apart the code to look nice
     code = code.trim().split("\n").map(r => r.trim()).filter(r => r.length).join(" ").split(" ");
+
+    // Create the basic output
     let compiledJS = " ".repeat(indents) + "// Rhein@2.0 Compiled code, isTypeStrict: " + isTypeStrict + "!\n";
+
+    // Compile everything
     while (code.length) {
+        // Get our current code
         let chunk = code.shift();
+
+        // Idk why I have to do this, but I do
         if (chunk.length < 1) {
             continue;
         }
+
+        // Hopefully, it's a command
         switch (chunk) {
             case "gpio": // GPIO command
                 break;
@@ -177,7 +211,8 @@ function compile(code, variables = {}, indents = 0, inLabel = false, isTypeStric
 
                 // Clean up the remaining code.
                 code = code.join(" ").replace(`label ${labelName}:`).replace(`label ${labelName};`).split(" ");
-            } break;
+            }
+            break;
             case "return": { // Return command
                 // If we aren't in a label, throw an error.
                 if (!inLabel) {
@@ -211,52 +246,54 @@ function compile(code, variables = {}, indents = 0, inLabel = false, isTypeStric
 
                 // Add the return statement to the compiled JS.
                 compiledJS += `${" ".repeat(indents)}/**\n${" ".repeat(indents + 1)}* @name ${inLabel} return value\n${" ".repeat(indents + 1)}* @type ${types[type].jsType}\n${" ".repeat(indents + 1)}*/\n${" ".repeat(indents)}return ${realValue};\n`;
-            } break;
+            }
+            break;
             default: {
                 if (types[chunk] !== undefined || !isTypeStrict) {
                     // Set up the proper type
-                let type = isTypeStrict ? types[chunk] : types.any,
-                typeName = isTypeStrict ? chunk : "any";
-            
-            // If we aren't in TypeStrict mode, let's get rid of the type requirement
-            if (isTypeStrict === false) {
-                if (chunk.length > 0) {
-                    code.unshift(chunk);
+                    let type = isTypeStrict ? types[chunk] : types.any,
+                        typeName = isTypeStrict ? chunk : "any";
+
+                    // If we aren't in TypeStrict mode, let's get rid of the type requirement
+                    if (isTypeStrict === false) {
+                        if (chunk.length > 0) {
+                            code.unshift(chunk);
+                        }
+                    }
+
+                    // Get the variable name
+                    let name = code.shift();
+
+                    // If the next thing isn't an "=", then we got an issue.
+                    if (code.shift() !== "=") {
+                        throw new Error("Invalid variable declaration!");
+                    }
+
+                    // Get the value of the variable
+                    let valueFound = getUntilNext(code, ";");
+
+                    // If it doesn't exist, throw an error.
+                    if (valueFound === -1) {
+                        throw new Error(`Cannot find value of variable ${name}!`);
+                    }
+
+                    // Extract the new code without the value, and the value.
+                    let [newCode, value] = valueFound;
+
+                    // Replace the new code with the old code, minus the value.
+                    code = newCode;
+
+                    // Get the value of the value.
+                    let realValue = getValue(value, variables, typeName);
+
+                    // Add stuff to the compiled code.
+                    compiledJS += `${" ".repeat(indents)}/**\n${" ".repeat(indents + 1)}* @name ${name}\n${" ".repeat(indents + 1)}* @type ${type.jsType}\n${" ".repeat(indents + 1)}*/\n${" ".repeat(indents)}let ${typeName}_${name} = ${realValue};\n`;
+
+                    // Add the new variable to the cache.
+                    variables[name] = new Variable(name, typeName, realValue);
                 }
             }
-
-            // Get the variable name
-            let name = code.shift();
-
-            // If the next thing isn't an "=", then we got an issue.
-            if (code.shift() !== "=") {
-                throw new Error("Invalid variable declaration!");
-            }
-
-            // Get the value of the variable
-            let valueFound = getUntilNext(code, ";");
-
-            // If it doesn't exist, throw an error.
-            if (valueFound === -1) {
-                throw new Error(`Cannot find value of variable ${name}!`);
-            }
-
-            // Extract the new code without the value, and the value.
-            let [newCode, value] = valueFound;
-
-            // Replace the new code with the old code, minus the value.
-            code = newCode;
-
-            // Get the value of the value.
-            let realValue = getValue(value, variables, typeName);
-
-            // Add stuff to the compiled code.
-            compiledJS += `${" ".repeat(indents)}/**\n${" ".repeat(indents + 1)}* @name ${name}\n${" ".repeat(indents + 1)}* @type ${type.jsType}\n${" ".repeat(indents + 1)}*/\n${" ".repeat(indents)}let ${typeName}_${name} = ${realValue};\n`;
-            
-            // Add the new variable to the cache.
-            variables[name] = new Variable(name, typeName, realValue);
-                }
-            } break;
+            break;
         }
     }
     return compiledJS.slice(0, -1).trimEnd();
